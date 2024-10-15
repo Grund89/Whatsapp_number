6cc859edc7cf23a476d989a54d4c80f2b340fa48
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-15 16:17:15 -0300

    feat: recria codigos

--- bin/check_whatsapp.rb
+++ bin/check_whatsapp.rb
@@ -1,15 +1,11 @@
-#!/usr/bin/env ruby
+require_relative '../lib/check_whatsapp/verifier'
 
-require "check_whatsapp"
+# Inicializa o Verifier com Account SID e Auth Token
+verifier = CheckWhatsApp::Verifier.new(ARGV[0], ARGV[1])
 
-if ARGV.length != 3
-  puts "Uso: check_whatsapp ACCOUNT_SID AUTH_TOKEN NUMERO_TELEFONE"
-  exit
-end
-
-account_sid = ARGV[0]
-auth_token = ARGV[1]
+# Passa o número de telefone para o método check_number
 phone_number = ARGV[2]
+resultado = verifier.check_number(phone_number)
 
-verifier = CheckWhatsApp::Verifier.new(account_sid, auth_token)
-puts verifier.check_number(phone_number)
+# Exibe o resultado
+puts resultado
