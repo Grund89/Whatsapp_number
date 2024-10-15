cc742820f4f3c0c555e25ba7707a07e954ff8099
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-15 15:07:22 -0300

    fix: substitui linha 4 do check_whatsapp.rb

--- lib/check_whatsapp.rb
+++ lib/check_whatsapp.rb
@@ -1,5 +1,5 @@
 require_relative 'check_whatsapp/verifier'
 
 # Passe os parâmetros para o Verifier
-verifier = CheckWhatsapp::Verifier.new(ARGV[0], ARGV[1], ARGV[2])
+verifier = CheckWhatsApp::Verifier.new(ARGV[0], ARGV[1], ARGV[2])
 verifier.check_number
