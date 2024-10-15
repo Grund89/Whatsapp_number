a12c2e8d2037ccc901b28b2478258118ed930593
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-15 15:14:19 -0300

    fix: informado apenas 2 argumentos para o metodo initilize

--- lib/check_whatsapp.rb
+++ lib/check_whatsapp.rb
@@ -1,5 +1,11 @@
-require_relative 'check_whatsapp/verifier'
+require_relative '../lib/check_whatsapp/verifier'
 
-# Passe os parâmetros para o Verifier
-verifier = CheckWhatsApp::Verifier.new(ARGV[0], ARGV[1], ARGV[2])
-verifier.check_number
+# Inicializa o Verifier com Account SID e Auth Token
+verifier = CheckWhatsApp::Verifier.new(ARGV[0], ARGV[1])
+
+# Passa o número de telefone para o método check_number
+phone_number = ARGV[2]
+resultado = verifier.check_number(phone_number)
+
+# Exibe o resultado
+puts resultado
