5efdbf78146a446f4741c39f084b8bbbedbef6fc
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-16 20:45:58 -0300

    feat: atualiza link API

--- lib/check_whatsapp/verifier.rb
+++ lib/check_whatsapp/verifier.rb
@@ -1,10 +1,10 @@
 require 'net/http'
 require 'json'
-require 'cgi'  # Adicione isso para usar CGI.escape
+require 'cgi'
 
 module CheckWhatsApp
   class Verifier
-    TWILIO_API_URL = 'https://api.twilio.com/2010-04-01/Accounts'
+    TWILIO_API_URL = 'https://lookups.twilio.com/v2/PhoneNumbers'
 
     def initialize(account_sid, auth_token)
       @account_sid = account_sid
@@ -12,23 +12,32 @@ module CheckWhatsApp
     end
 
     def check_number(phone_number)
-      # Substituímos URI.encode por CGI.escape
       encoded_phone_number = CGI.escape(phone_number)
-      uri = URI("#{TWILIO_API_URL}/#{@account_sid}/IncomingPhoneNumbers/#{encoded_phone_number}.json")
+      uri = URI("#{TWILIO_API_URL}/#{encoded_phone_number}")
+      response = make_request(uri)
+
+      case response
+      when Net::HTTPSuccess
+        data = JSON.parse(response.body)
+        return has_whatsapp?(data) ? "Número possui WhatsApp" : "Número não possui WhatsApp"
+      else
+        return "Erro na verificação: #{response.message}"
+      end
+    end
+
+    private
+
+    def make_request(uri)
       req = Net::HTTP::Get.new(uri)
       req.basic_auth(@account_sid, @auth_token)
 
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)
       end
+    end
 
-      case res
-      when Net::HTTPSuccess
-        data = JSON.parse(res.body)
-        return data["whatsapp"] ? "Número possui WhatsApp" : "Número não possui WhatsApp"
-      else
-        return "Erro na verificação: #{res.message}"
-      end
+    def has_whatsapp?(data)
+      data.key?("capabilities") && data["capabilities"]["sms"]
     end
   end
 end
