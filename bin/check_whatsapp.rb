a800972c00df7b8102058516ae970843b5b577d5
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-16 21:25:03 -0300

    feat: atualiza URL AZPI

--- bin/check_whatsapp.rb
+++ bin/check_whatsapp.rb
@@ -1,11 +1,7 @@
 require_relative '../lib/check_whatsapp/verifier'
 
-# Inicializa o Verifier com Account SID e Auth Token
-verifier = CheckWhatsApp::Verifier.new(ARGV[0], ARGV[1])
+# Atribuindo apenas o token da API
+verifier = CheckWhatsApp::Verifier.new(ARGV[0])
 
-# Passa o número de telefone para o método check_number
-phone_number = ARGV[2]
-resultado = verifier.check_number(phone_number)
-
-# Exibe o resultado
-puts resultado
+# Verificando o número passado como argumento
+puts verifier.check_number(ARGV[1])
--- lib/check_whatsapp/verifier.rb
+++ lib/check_whatsapp/verifier.rb
@@ -3,27 +3,30 @@ require 'json'
 
 module CheckWhatsApp
   class Verifier
-    Z_API_URL = 'https://api.z-api.io/instances/seu_id_da_instance/token/seu_token_aqui/contacts'
+    ZAPI_API_URL = 'https://api.z-api.io/instances/3D6E48EB6E3900C780172272D360C213/token/6775DEBE21946BD9CBD6E017/send-text'
 
     def initialize(api_token)
       @api_token = api_token
     end
 
     def check_number(phone_number)
-      uri = URI("#{Z_API_URL}/#{phone_number}/iswhatsapp")
+      uri = URI("#{ZAPI_API_URL}/#{phone_number}/iswhatsapp")
       req = Net::HTTP::Get.new(uri)
+      req['Authorization'] = "Bearer #{@api_token}"
 
       res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)
       end
 
-      # Verifique se a resposta é válida e se o corpo pode ser convertido em JSON
+      # Exibe a resposta completa para depuração
+      puts "Resposta da API: #{res.body}"
+
       if res.is_a?(Net::HTTPSuccess)
         data = JSON.parse(res.body)
 
-        # Verifique se o campo esperado está presente no corpo da resposta
-        if data && data["isWhatsapp"]
-          return data["isWhatsapp"] ? "Número possui WhatsApp" : "Número não possui WhatsApp"
+        # Verifica se a estrutura esperada está presente
+        if data && data["result"] && data["result"]["isWhatsapp"]
+          return data["result"]["isWhatsapp"] ? "Número possui WhatsApp" : "Número não possui WhatsApp"
         else
           return "Erro: Resposta da API não contém os dados esperados."
         end
