ed3f74728a4f6815f276af507b462a1f96403a31
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-16 21:19:33 -0300

    feat: troca de api

--- lib/check_whatsapp/verifier.rb
+++ lib/check_whatsapp/verifier.rb
@@ -1,43 +1,35 @@
 require 'net/http'
 require 'json'
-require 'cgi'
 
 module CheckWhatsApp
   class Verifier
-    TWILIO_API_URL = 'https://lookups.twilio.com/v2/PhoneNumbers'
+    Z_API_URL = 'https://api.z-api.io/instances/seu_id_da_instance/token/seu_token_aqui/contacts'
 
-    def initialize(account_sid, auth_token)
-      @account_sid = account_sid
-      @auth_token = auth_token
+    def initialize(api_token)
+      @api_token = api_token
     end
 
     def check_number(phone_number)
-      encoded_phone_number = CGI.escape(phone_number)
-      uri = URI("#{TWILIO_API_URL}/#{encoded_phone_number}")
-      response = make_request(uri)
-
-      case response
-      when Net::HTTPSuccess
-        data = JSON.parse(response.body)
-        return has_whatsapp?(data) ? "Número possui WhatsApp" : "Número não possui WhatsApp"
-      else
-        return "Erro na verificação: #{response.message}"
-      end
-    end
-
-    private
-
-    def make_request(uri)
+      uri = URI("#{Z_API_URL}/#{phone_number}/iswhatsapp")
       req = Net::HTTP::Get.new(uri)
-      req.basic_auth(@account_sid, @auth_token)
 
-      Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)
       end
-    end
 
-    def has_whatsapp?(data)
-      data.key?("capabilities") && data["capabilities"]["sms"]
+      # Verifique se a resposta é válida e se o corpo pode ser convertido em JSON
+      if res.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(res.body)
+
+        # Verifique se o campo esperado está presente no corpo da resposta
+        if data && data["isWhatsapp"]
+          return data["isWhatsapp"] ? "Número possui WhatsApp" : "Número não possui WhatsApp"
+        else
+          return "Erro: Resposta da API não contém os dados esperados."
+        end
+      else
+        return "Erro na verificação: #{res.message}"
+      end
     end
   end
 end
